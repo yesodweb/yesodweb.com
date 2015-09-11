82dcc034fca77c44b1c4df923df98e09eab3ceb8
Michael Snoyman <michael@snoyman.com>
2015-09-11 09:47:21 +0300

    Move to yesod-cookbook repo (pinging @psibi)

--- Handler/Wiki.hs
+++ Handler/Wiki.hs
@@ -8,7 +8,11 @@ import Network.HTTP.Types (status301)
 import qualified Data.Text as T
 
 getWikiHomeR :: Handler Html
-getWikiHomeR = redirectWith status301 ("https://github.com/yesodweb/yesod/wiki" :: T.Text)
+getWikiHomeR = redirect ("https://github.com/yesodweb/yesod-cookbook" :: T.Text)
 
 getWikiR :: Text -> Handler Html
-getWikiR path = redirectWith status301 $ "https://github.com/yesodweb/yesod/wiki/" `T.append` path
+getWikiR path = redirect $ T.concat
+    [ "https://github.com/yesodweb/yesod-cookbook/blob/master/cookbook/"
+    , path
+    , ".md"
+    ]
